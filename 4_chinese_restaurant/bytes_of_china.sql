731db38d92556bf5be84212c614afe932a7a03c0
Eduardo Rojas <carlos.edhr@protonmail.com>
2023-06-20 01:20:57 -0700

    Completed Task #3 by adding the sample data and correcting schema errors.

--- 4_chinese_restaurant/bytes_of_china.sql
+++ 4_chinese_restaurant/bytes_of_china.sql
@@ -2,12 +2,11 @@
 -- -- Create restaurant and address tables
 CREATE TABLE restaurant(
     id integer primary key,
-    name varchar(100), 
-    description varchar(350),
-    hours varchar(350),
-    telephone char(10),
-    ratings decimal
-
+    name varchar(20), 
+    description varchar(100),
+    rating decimal,
+    telephone char(15),
+    hours varchar(350)
 );
 
 CREATE TABLE address(
@@ -16,7 +15,7 @@ CREATE TABLE address(
     street_name varchar(20),
     city varchar(20),
     state varchar(15),
-    google_maps varchar(150)
+    google_map_link varchar(150)
 );
 
 -- -- Validate that primary keys exist for restaurant and address tables
@@ -40,8 +39,7 @@ WHERE table_name = 'category' ;
 -- --Create table for dishes
 CREATE TABLE dish(
     id integer primary key, 
-    name varchar(30),
-    price money,
+    name varchar(50),
     description varchar(250),
     hot_and_spicy boolean
 );
@@ -54,8 +52,8 @@ WHERE table_name = 'dish' ;
 -- --Create table for customer reviews
 CREATE TABLE review(
     id integer primary key,
-    customer_review varchar(350),
     rating decimal,
+    description varchar(350),
     date date
 );
 
@@ -70,13 +68,13 @@ WHERE table_name = 'review' ;
 
 -- -- ONE TO ONE RELATIONSHIP
 -- -- Restaurant - Address
-ALTER TABLE restaurant 
-ADD address_id INTEGER UNIQUE;
+ALTER TABLE address 
+ADD restaurant_id INTEGER UNIQUE;
 
-ALTER TABLE restaurant
+ALTER TABLE address
 ADD CONSTRAINT fk_restaurant_address 
-FOREIGN KEY (address_id) 
-REFERENCES address(id) ;
+FOREIGN KEY (restaurant_id) 
+REFERENCES restaurant(id) ;
 
 -- -- Validate that primary keys exist for restaurant table
 SELECT constraint_name, table_name,column_name 
@@ -104,6 +102,7 @@ WHERE table_name = 'review' ;
 CREATE TABLE categories_dishes(
     category_id char(2) REFERENCES category(id),
     dish_id integer REFERENCES dish(id),
+    price money,
     PRIMARY KEY (category_id, dish_id)
 );
 
@@ -111,3 +110,203 @@ CREATE TABLE categories_dishes(
 SELECT constraint_name, table_name, column_name 
 FROM information_schema.key_column_usage 
 WHERE table_name = 'dish' OR table_name = 'category' ;
+
+
+-- -- Section 3: Insert Sample Data
+/* 
+ *--------------------------------------------
+ Insert values for restaurant
+ *--------------------------------------------
+ */
+INSERT INTO restaurant VALUES (
+  1,
+  'Bytes of China',
+  'Delectable Chinese Cuisine',
+  3.9,
+  '6175551212',
+  'Mon - Fri 9:00 am to 9:00 pm, Weekends 10:00 am to 11:00 pm'
+);
+
+/* 
+ *--------------------------------------------
+ Insert values for address
+ *--------------------------------------------
+ */
+INSERT INTO address VALUES (
+  1,
+  '2020',
+  'Busy Street',
+  'Chinatown',
+  'MA',
+  'http://bit.ly/BytesOfChina',
+  1
+);
+
+/* 
+ *--------------------------------------------
+ Insert values for review
+ *--------------------------------------------
+ */
+INSERT INTO review VALUES (
+  1,
+  5.0,
+  'Would love to host another birthday party at Bytes of China!',
+  '05-22-2020',
+  1
+);
+
+INSERT INTO review VALUES (
+  2,
+  4.5,
+  'Other than a small mix-up, I would give it a 5.0!',
+  '04-01-2020',
+  1
+);
+
+INSERT INTO review VALUES (
+  3,
+  3.9,
+  'A reasonable place to eat for lunch, if you are in a rush!',
+  '03-15-2020',
+  1
+);
+
+/* 
+ *--------------------------------------------
+ Insert values for category
+ *--------------------------------------------
+ */
+INSERT INTO category VALUES (
+  'C',
+  'Chicken',
+  null
+);
+
+INSERT INTO category VALUES (
+  'LS',
+  'Luncheon Specials',
+  'Served with Hot and Sour Soup or Egg Drop Soup and Fried or Steamed Rice  between 11:00 am and 3:00 pm from Monday to Friday.'
+);
+
+INSERT INTO category VALUES (
+  'HS',
+  'House Specials',
+  null
+);
+
+/* 
+ *--------------------------------------------
+ Insert values for dish
+ *--------------------------------------------
+ */
+INSERT INTO dish VALUES (
+  1,
+  'Chicken with Broccoli',
+  'Diced chicken stir-fried with succulent broccoli florets',
+  false
+);
+
+INSERT INTO dish VALUES (
+  2,
+  'Sweet and Sour Chicken',
+  'Marinated chicken with tangy sweet and sour sauce together with pineapples and green peppers',
+  false
+);
+
+INSERT INTO dish VALUES (
+  3,
+  'Chicken Wings',
+  'Finger-licking mouth-watering entree to spice up any lunch or dinner',
+  true
+);
+
+INSERT INTO dish VALUES (
+  4,
+  'Beef with Garlic Sauce',
+  'Sliced beef steak marinated in garlic sauce for that tangy flavor',
+  true
+);
+
+INSERT INTO dish VALUES (
+  5,
+  'Fresh Mushroom with Snow Peapods and Baby Corns',
+  'Colorful entree perfect for vegetarians and mushroom lovers',
+  false
+);
+
+INSERT INTO dish VALUES (
+  6,
+  'Sesame Chicken',
+  'Crispy chunks of chicken flavored with savory sesame sauce',
+  false
+);
+
+INSERT INTO dish VALUES (
+  7,
+  'Special Minced Chicken',
+  'Marinated chicken breast sauteed with colorful vegetables topped with pine nuts and shredded lettuce.',
+  false
+);
+
+INSERT INTO dish VALUES (
+  8,
+  'Hunan Special Half & Half',
+  'Shredded beef in Peking sauce and shredded chicken in garlic sauce',
+  true
+);
+
+/*
+ *--------------------------------------------
+ Insert valus for cross-reference table, categories_dishes
+ *--------------------------------------------
+ */
+INSERT INTO categories_dishes VALUES (
+  'C',
+  1,
+  6.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'C',
+  3,
+  6.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'LS',
+  1,
+  8.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'LS',
+  4,
+  8.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'LS',
+  5,
+  8.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'HS',
+  6,
+  15.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'HS',
+  7,
+  16.95
+);
+
+INSERT INTO categories_dishes VALUES (
+  'HS',
+  8,
+  17.95
+);
+
+
+-- -- Section 4: Make Sample Queries
