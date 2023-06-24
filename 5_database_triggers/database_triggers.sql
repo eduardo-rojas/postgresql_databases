ba70c950d70d00dc598a161cc72f4381a8832576
Eduardo Rojas <carlos.edhr@protonmail.com>
2023-06-23 22:12:08 -0700

    Initialized project #5, database triggers.

---
+++ 5_database_triggers/database_triggers.sql
@@ -0,0 +1,8 @@
+/* DATABASE TRIGGERS  */
+
+/* There are two tables we will be working with: 
+customers and customers_log. To make your life 
+easier we would recommend ordering the customers 
+table by customer_id
+*/
+
